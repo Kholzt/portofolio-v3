5a30c9eeadc7dbb1dea4bf3a8534373eaf7461d5
Kholzt <99956928+Kholzt@users.noreply.github.com>
2025-04-04 19:16:44 +0700

    update

--- app/Filament/Resources/AchievementsResource.php
+++ app/Filament/Resources/AchievementsResource.php
@@ -37,14 +37,14 @@ class AchievementsResource extends Resource
                     ->visibility('public')
                     ->disk("public")
                     ->acceptedFileTypes(["image/*"])
-                    ->directory("projek"),
+                    ->directory("Achievement"),
                 FileUpload::make('attachment2')->nullable()
                     ->image()
                     ->imagePreviewHeight("150") // Menampilkan preview gambar
                     ->visibility('public')
                     ->disk("public")
                     ->acceptedFileTypes(["image/*"])
-                    ->directory("projek"),
+                    ->directory("Achievement"),
             ]);
     }
     protected function getCreateAction(): Action
